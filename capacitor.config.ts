2d807bd469d1ee723c70a36970794ab719cbcfec
Nur Amanina <20ftt1456@student.pb.edu.bn>
2022-08-03 22:44:16 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ITS11',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
